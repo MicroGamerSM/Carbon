8cef676e86db277e90244ce6f439ff20e65d777d
MicroGamerSM <98235195+MicroGamerSM@users.noreply.github.com>
2023-05-06 19:57:23 -0400

    Added Vector2 and Zone2

---
+++ main.ts
@@ -0,0 +1,35 @@
+export class vector2 {
+	x:number
+	y:number
+	
+	deconstruct () {
+		return this.x, this.y
+	}
+	
+	constructor (x:number, y:number) {
+		this.x = x
+		this.y = y
+	}
+}
+export class zone2 {
+	x:number
+	y:number
+	w:number
+	h:number
+	
+	pointisInside (point:vector2):boolean {
+		var x = point.x > this.x && this.x + this.w > point.x
+		var y = point.y > this.y ** this.y + this.h > point.y
+		return x && y
+	}
+	
+	deconstruct () {
+		returh this.x, this.y, this.w, this.h
+	}
+	
+	constructor (x:number, y:number, w:number, h:number) {
+		this.x = x
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
